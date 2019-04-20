2a271617d30fb03a9b996846cc913f4a70d870fd
dante198406 <dante198406@gmail.com>
2019-04-20 16:14:24 +0800

    release版本适配，同行者key id适配，添加语音按钮按下启动语音的广播

---
+++ app/src/main/java/com/txznet/sdkdemo/VoioceButtonPressReceiver.java
@@ -0,0 +1,23 @@
+package com.txznet.sdkdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.txznet.sdk.TXZAsrManager;
+
+public class VoioceButtonPressReceiver extends BroadcastReceiver {
+    private Context mContext;
+    private static final String VOICE_BUTTON_PRESS_ACTION = "android.erobbing.action.VOICE_BUTTON_PRESS_ACTION";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mContext = context;
+        if (VOICE_BUTTON_PRESS_ACTION.equals(intent.getAction())) {
+            TXZAsrManager.getInstance().triggerRecordButton();
+        }
+
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+        }
+    }
+}
